1caa908b6eb55ed752f1c56071b5823e18b7580a
yizhong.liao <yizhong.liao@jcipn.com>
2021-08-30 16:20:37 +0800

    初始化项目

---
+++ youyaun-admin/src/main/java/com/youyuan/YouyaunAdminApplication.java
@@ -0,0 +1,13 @@
+package com.youyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouyaunAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YouyaunAdminApplication.class, args);
+    }
+
+}
